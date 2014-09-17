6c7d661bb10f41497b3ffa1e215db02c990fc4f6
asif chowdhury <asif.chowdhury64@gmail.com>
2014-09-17 17:50:47 -0500

    updated netflix

--- Netflix.py
+++ Netflix.py
@@ -5,25 +5,54 @@ def netflix_read (r) :
         return
       return line
 
+def predict_ratings(a):
+    return 3
+
+def rmse (r, p):
+    assert (len(p) == len(r))
+    sum = 0
+    for i in range (len(p)):
+      sq_diff = (r[i] - p[i]) ** 2
+      sum += sq_diff
+    mean = sum / len(r)
+    sq_root = mean ** .5
+    return sq_root
+
 def netflix_print (w, v) :
     w.write(str(v) + "\n")
 
 def netflix_solve (r, w) :
+    ## this list is used to print movie ratings
     cashe = []
+    ## this list is used as an actual cashe
+    c_public = []
+    ## this list is used to skip movies to calculate rmse
+    skip_movies = []
     l = " "
     while True :
         a = netflix_read(r)        
         if not a :
           for i in range (len(cashe)):
             netflix_print(w, cashe[i])
+
+          rms = rmse(c_public, skip_movies)
+          netflix_print(w, rms)
           return
         else:
           f = a.find(":")
           if f < 0 :
-            cashe.append(3)
+            p = predict_ratings(a)
+            c_public.append(4)
+            cashe.append(p)
+            skip_movies.append(p)
           else:
-            
             cashe.append(a)
+            
+#        x = len(c_public)
+#        y = len(cashe)
+#        print(x, y)
+#    rms = rmse(c_public, cashe)
+#    netflix_print(w, rms)
 
 
 import sys
