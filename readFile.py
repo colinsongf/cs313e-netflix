eee10f56eb25b98ac267333179f00c4a5a35d8a5
hyunji-seo <hjseo767@gmail.com>
2014-09-18 17:26:14 -0500

    updated cache

--- readFile.py
+++ readFile.py
@@ -1,36 +1,39 @@
 import math
 import string
-def collatz_read (r) :
-    
+
+def netflix_read (r) :
     s = r.readline()
+    # use s[-1:]? m = s[:-1]
+    # m = movie ID
     z = s.find(":")
     if z > -1:
         m = s[:z]
-    sum = 0
-    count = 0
+
+    sum_a = 0
+    count_a = 0
     for line in r:
       line = line.strip()
+      # a = ratings for given customer
       a = ""
       f = line.find(",")
+
       if s == "" :
         return []
+
       elif f > -1:
-        count +=1
+        count_a +=1
         a = line[f+1]
-        
-        sum += int(a)
-    return int(m), sum/count
+        sum_a += int(a)
+    # rmr to use python3
+    return int(m), sum_a/count_a
 
 def main():
-  
-  for i in range (1, 17771):
-    x = ""
+  for i in range (1, 9):
     x = "/u/downing/cs/netflix/training_set/mv_"+str(i).zfill(7)+".txt"
     
     opn = open (x, "r")
-    s, y = collatz_read(opn)
+    s, y = netflix_read(opn)
     
-    print (s, round(y,1))
+    print (s, round(y, 1))
     opn.close()
-
 main()
