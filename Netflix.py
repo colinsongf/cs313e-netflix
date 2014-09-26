bd99d3c61acc61710dd8c7bf824d31862f26295b
Hyunji Seo <hyunji-seo@users.noreply.github.com>
2014-09-25 22:16:00 -0500

    Update Netflix.py

--- Netflix.py
+++ Netflix.py
@@ -15,12 +15,7 @@ def predict_offset(a_rating, overall_mean):
 
 def rmse (r, p):
   for i in range (len(p)):
-    f = p[i].find(":")
-    g = r[i].find(":")
-    if ((f < 0) & (g < 0)):
-      f = p[i]
-      g = r[i]
-      s = sum(map(lambda x, y: (x-y) ** 2, g, f))
+      s = sum(map(lambda x, y: (x-y) ** 2, r, p))
   return (s/len(r)) ** .5
 
 def netflix_print (w, v) :
@@ -28,29 +23,19 @@ def netflix_print (w, v) :
 
 def netflix_solve (r, w) :
 
-
-  # list_movie = search the input movie in c_movie
-  # lists all the ratings for individual movie
-  list_movie = []
-
-  # list_user = search the input user in c_user
-  # lists all the ratings for individual user
-  
-
 # -------------- "actual" has actual output -------------------------------------------------------------------
   id_rtg = []
   actual = {}
-  actual_rtgs = open("hs9234-probe_mv_rtg.txt", "r")
+  actual_rtgs = open("testing.txt", "r")
   for line in actual_rtgs:    
     line = line.strip()
     f = line.find(":")
     if f < 0:
       id_rtg.append(line)
     else:
-      actual[line] = id_rtg
+      actual[int(line[:f])] = id_rtg
       id_rtg = []
-#  print(actual)
-#  print(actual['1:'])
+
 # -------------- dict_user has customer IDs as keys & average rtgs of each customer as values--------------------------------------------------
   dict_user = {}
   c_user = open("ctd446-userAverageRating.txt", "r")
@@ -79,6 +64,7 @@ def netflix_solve (r, w) :
 # --------------------------------------------------------------------------------------------------------------------------------------
   
   probe = []
+  list_movie = []
 
   # u_offset = dictionary for user_offset with user ID as keys
   u_offset = []
@@ -103,7 +89,7 @@ def netflix_solve (r, w) :
             ## "if" runs if each_element contains a movie ID
             if f > -1:
                movie_id = int(element[:f])
-               our_predict.append(element)
+             #  our_predict.append(element)
                netflix_print(w, element)       
             ## uses the movie-id from "if" statement & uses that movie_id to calculate movie offset
             else:
@@ -111,7 +97,7 @@ def netflix_solve (r, w) :
                movie_offset = predict_offset(dict_movie[movie_id], movie_mean)
                               
                ## element = user_offset
-               final_prediction =  movie_mean + float(element) + movie_offset
+               final_prediction =  movie + float(element) + movie_offset
                
                ## our_predict_m = list of predicted ratings with movie_offset, user_offset & user_mean
                if final_prediction > 5:
@@ -119,11 +105,11 @@ def netflix_solve (r, w) :
                if final_prediction < 2:
                    final_prediction = 2
               
-               our_predict.append(str(final_prediction))     	       
-#               netflix_print(w, (round(float(final_prediction), 1)))
-#      print(len(list_movie), len(our_predict))
+               our_predict.append(float(final_prediction))     	       
+               netflix_print(w, (round(float(final_prediction), 1)))
+
       rms2 = rmse(list_movie, our_predict)
-      print("RMSE:", rms2)
+      print("rmse:", rms2)
       end = time.time()
       print(end-start)
       return
@@ -135,10 +121,8 @@ def netflix_solve (r, w) :
           # a = customer ID for this if statement
           for id in list_user:
             f = id.find(" ")
-#            print("a", a, "id", id[:f])
             if a == id[:f]:
-               list_movie.append(id[f+1:])
-               # list_movie supposed to have actual ratings for inputted customer ids & movie ids
+               list_movie.append(float(id[f+1:]))
               
           ## calculates user offset
           user_offset = predict_offset(dict_user[a], user_mean)
@@ -149,11 +133,10 @@ def netflix_solve (r, w) :
       ## this "else" runs if it finds the movie ID in input     
       else:
           list_user = []
-          # list_user has all the customer ID & actual ratings for a specific "a = movie_id"
-          list_user = actual[str(a)]
-          list_movie.append(a)
           u_offset.append(a)
-          
-  
+          # list_user has all the customer ID & actual ratings for a specific "a = movie_id"
+          a = a[:f]
+          list_user = actual[int(a)]
+
 netflix_solve(sys.stdin, sys.stdout)
 
