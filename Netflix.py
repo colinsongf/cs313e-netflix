e8b565225d4cd5f4fd6d2e895266f1a78bfd7f90
asif chowdhury <asif.chowdhury64@gmail.com>
2014-09-30 14:31:28 -0500

    finally under 1 min

--- Netflix.py
+++ Netflix.py
@@ -1,9 +1,10 @@
-import time
 import math
+import pickle
 def netflix_read (r) :
   for line in r:
     line = line.strip()
-    if line == " " :
+    f = line.find(" ")
+    if f > -1:
       return
     return line
 
@@ -13,47 +14,64 @@ def netflix_print (w, v) :
 def rmse (r, p):
   return (sum(map(lambda x, y: (x-y) ** 2, r, p)) / len(p)) ** .5
 
-#def rms (r, p):
-#  z = zip(r,p)
-#  v = sum([(x - y) ** 2 for x, y in z])
-#  return math.sqrt(v // len(r))
-
 def netflix_solve (r, w) :
-  start = time.time()
-  probe_ans = eval(open("probe_dict.txt", "r").readline())
-  dict_user = eval(open("predct.txt", "r").readline())
+
+  probe_ans = {}
+  probe_ans = pickle.load( open( "hs9234_probe_ans.p", "rb" ) )
+
+  dict_user = {}
+  dict_user = pickle.load( open( "hs9234_offsets.p", "rb" ) )
 
 # --------------------------------------------------------------------------------------------------------------------------------------
-  movie = []
-  predict = []
-#  actual_id = 0
-#  offst = 0
-#  d = {}
-#  offset = {}
-  count = 0
-#  sum = 0
+  probe = []
   while True :
-      a = netflix_read(r)
-      if not a:
-        rms = format((rmse(movie, predict)), ".2f")
+    a = netflix_read(r) 
+
+    ## this "if" runs after reading all the input       
+    if not a:
+      if len(probe) > 30000:
+        m = 0
+        sum = 0
+        count = 0
+        
+        for i in range (len(probe)):
+          
+          if probe[i][-1:] == ":":
+            m = int(probe[i][:-1])
+            netflix_print(w, probe[i])
+          else:
+            count += 1
+            actual_id = probe_ans[m][int(probe[i])]
+            offst = dict_user[m][int(probe[i])]
+            prediction = 3.3 + offst
+            sum += (actual_id - prediction) ** 2
+            netflix_print(w, round(prediction, 1))
+
+        rms = (sum / count) ** .5
         print("RMSE:", rms)
-        end = time.time()
-        print(end-start)
-        return
+        return      
+
       else:
-        f = a.find(":")
-        if f > -1:
-          m = int(a[:f])
-          netflix_print(w, a)
-        #  d = probe_ans[m]
-        #  offset = dict_user[m]
-        else: 
-          count += 1
-          actual_id = probe_ans[m][int(a)]
-          offst = dc[m][int(a)]
-          # prediction = 3.3 + offst
-        #  if prediction > 5:
-         #      prediction = 5
-          movie.append(actual_id)
-          predict.append(offst)
-          netflix_print(w, offst)
+        m = 0
+        sum = 0
+        count = 0
+        
+        for i in range (len(probe)):
+          
+          if probe[i][-1:] == ":":
+            m = int(probe[i][:-1])
+            netflix_print(w, probe[i])
+          else:
+            count += 1
+            actual_id = probe_ans[m][int(probe[i])]
+            offst = dict_user[m][int(probe[i])]
+            prediction = 3.3 + offst
+            sum += (actual_id - prediction) ** 2
+            netflix_print(w, round(prediction, 1))
+
+        rms = (sum // count) ** .5
+        print("RMSE:", format(rms, ".2f"))
+        return      
+      
+    else:
+      probe.append(a)
